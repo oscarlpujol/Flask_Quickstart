e404416b5236023438b186bd3fa960895d731e09
oscarlpujol <oscarlahuerta@gmail.com>
2023-12-18 23:05:35 +0100

    Adding POST route to change JSON file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -12,4 +12,15 @@ def render_test():
     if request.method == "GET":
         with open("./data/parameters.json") as file:
             parameters = json.load(file)
+        print(parameters)
         return render_template("test.html", parameters=parameters)
+    
+@app.route("/test", methods=["POST"])
+def change_parameter():
+    if request.method == "POST":
+        print(request.form)
+        with open("./data/parameters.json", 'w') as file:
+            param = { "test" : request.form["test"]}
+            file.write(json.dumps(param, ensure_ascii=False))
+
+        return redirect(url_for("render_test"))
